101171300a58704fe8851dd9d63622146a8a2ad9
Artsiom <artsiom.hurynovich@gmail.com>
2025-02-07 10:22:10 +0100

    Initial Commit

---
+++ TrafficLight/BlueButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  BlueButtonView.swift
+//  TrafficLight
+//
+//  Created by Artem H on 2/11/25.
+//
+
+import SwiftUI
+
+struct BlueButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BlueButtonView()
+}
---
+++ TrafficLight/LightCircleView.swift
@@ -0,0 +1,8 @@
+//
+//  LightCircleView.swift
+//  TrafficLight
+//
+//  Created by Artem H on 2/7/25.
+//
+
+import Foundation
